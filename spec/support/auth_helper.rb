f2d75cf0186be54cff358a28725750135084775e
Julien P.Lefebvre <julienplefebvre@gmail.com>
2020-11-21 13:22:42 -0500

    rubocop lint applied on all files
    except missing first line comment
    
    brakeman gem added

--- spec/support/auth_helper.rb
+++ spec/support/auth_helper.rb
@@ -1,10 +1,11 @@
+# frozen_string_literal: true
+
 module AuthHelper
   def login_user
     # Before each test, create and login the user
     before(:each) do
-      @request.env["devise.mapping"] = Devise.mappings[:user]
+      @request.env['devise.mapping'] = Devise.mappings[:user]
       @user = FactoryBot.create(:user)
-      # user.confirm! # Or set a confirmed_at inside the factory. Only necessary if you are using the "confirmable" module
       sign_in @user
     end
   end
@@ -12,12 +13,4 @@ module AuthHelper
   def logout_user
     sign_out @user if @user
   end
-
-  # Not used in this tutorial, but left to show an example of different user types
-  # def login_admin
-  #   before(:each) do
-  #     @request.env["devise.mapping"] = Devise.mappings[:admin]
-  #     sign_in FactoryBot.create(:admin) # Using factory bot as an example
-  #   end
-  # end
 end
