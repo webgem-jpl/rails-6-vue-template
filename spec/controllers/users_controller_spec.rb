b2bc3493b79e977f4cd738585ceb597a3f40d105
Julien P.Lefebvre <julienplefebvre@gmail.com>
2020-11-21 13:17:52 -0500

    config: rubocop installed

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -2,39 +2,34 @@ require 'rails_helper'
 
 # Change this ArticlesController to your project
 RSpec.describe UsersController, type: :controller do
-
   # This should return the minimal set of attributes required to create a valid
   # Article. As you add validations to Article, be sure to adjust the attributes here as well.
-  let(:valid_attributes) {
-      {:email => "jean-claude.vandam@gmail.com", :password => "qwerty" }
-  }
-
+  let(:valid_attributes) do
+    { email: 'jean-claude.vandam@gmail.com', password: 'qwerty' }
+  end
 
   let(:valid_session) { {} }
 
-  describe "GET #index" do
-    context "user is logged in" do
+  describe 'GET #index' do
+    context 'user is logged in' do
       login_user
 
-      it "returns a success response - when sign in" do
-          User.create!(valid_attributes)
-          get :index, params: {}, session: valid_session
-          expect(response).to be_successful
+      it 'returns a success response - when sign in' do
+        User.create!(valid_attributes)
+        get :index, params: {}, session: valid_session
+        expect(response).to be_successful
       end
-
     end
   end
 
-  describe "GET #index" do
-    context "user is logged out" do
+  describe 'GET #index' do
+    context 'user is logged out' do
       logout_user
 
-      it "redirect to sign in page" do
-          get :index, params: {}
-          expect(response).to have_http_status(302)
+      it 'redirect to sign in page' do
+        get :index, params: {}
+        expect(response).to have_http_status(302)
       end
-      
     end
   end
-
 end
